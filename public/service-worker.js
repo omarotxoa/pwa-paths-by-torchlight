a3ccbdc05dfcda3f0e530bb5cec79f0496a0b1bc
Omar Ochoa <omar@ochoawebdesign.com>
2023-05-07 22:59:17 -0700

    SW: Installs service worker with basic cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,21 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register("/serviceworker.js");
-}
+self.addEventListener("install", e => {
+  e.waitUntil(
+      caches.open("pwa-assets").then(cache => {
+          return cache.addAll([
+              "./", 
+              "styles.css", 
+              "script.js", 
+              "../src/icon-96.png", 
+              "../src/icon-256.png"
+          ]);
+      })
+  ); 
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+      caches.match(e.request).then(response => {
+          return response || fetch(e.request);
+      })
+  );
+});
